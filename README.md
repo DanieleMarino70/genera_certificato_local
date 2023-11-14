7fcef9c0fa7e20e5837559d8ceafee73027ec473
Daniele Marino <d.marino@linkmes.it>
2023-11-14 16:31:21 +0100

    fix: reconstructured

--- README.md
+++ README.md
@@ -5,14 +5,37 @@ Questo progetto Python consente di generare chiavi private, richieste di certifi
 ## Istruzioni
 
 1. **Clona il repository o copia i file nel tuo progetto.**
-2. **Assicurati di avere Python installato sul tuo sistema.**
+2. **Assicurati di avere Python installato sul tuo sistema e Git**
 3. **Installazione delle dipendenze:**
-   
-    ```bash
-    pip install pyopenssl
-    ```
+4. **Modifica del file openssl-custom.cnf**
+    *modifiche da non effettuare contrassegnate con X*
+    ```bash 
+        [req]
+        default_bits = 2048 - X
+        prompt = no - X
+        default_md = sha256 - X
+        x509_extensions = v3_req - X
+        distinguished_name = dn - X
 
-4. **Esecuzione dello script:**
+        [dn]
+        C = US
+        ST = KS
+        L = Olathe
+        O = IT
+        OU = IT Department
+        emailAddress = webmaster@example.com
+        CN = localhost
+
+        [v3_req]
+        subjectAltName = @alt_names
+
+        [alt_names]
+        DNS.1 = *.localhost - X
+        DNS.2 = localhost - X
+    ```
+  
+
+5. **Esecuzione dello script:**
    
     ```bash
     python genera_cert.py
@@ -30,9 +53,43 @@ Questo progetto Python consente di generare chiavi private, richieste di certifi
 - `genera_cert.py`: Lo script principale per la generazione di chiavi private, CSR, e certificati.
 - `certificati/`: La cartella in cui vengono salvati i certificati generati.
 
+## Istruzioni
+```bash
+OS X
+	1. Double click on the certificate (server.crt)
+	2. Select your desired keychain (login should suffice)
+	3. Add the certificate
+	4. Open Keychain Access if it isn’t already open
+	5. Select the keychain you chose earlier
+	6. You should see the certificate localhost
+	7. Double click on the certificate
+	8. Expand Trust
+	9. Select the option Always Trust in When using this certificate
+	10. Close the certificate window
+
+The certificate is now installed.
+
+
+Windows 10
+
+	1. Double click on the certificate (server.crt)
+	2. Click on the button “Install Certificate …”
+	3. Select whether you want to store it on user level or on machine level
+	4. Click “Next”
+	5. Select “Place all certificates in the following store”
+	6. Click “Browse”
+	7. Select “Trusted Root Certification Authorities”
+	8. Click “Ok”
+	9. Click “Next”
+	10. Click “Finish”
+
+If you get a prompt, click “Yes”
+```
 ## Nota
 
 Questo script genera certificati autofirmati, adatti solo a scopi di sviluppo o testing. Per ambienti di produzione, è consigliabile ottenere certificati da un'autorità di certificazione affidabile.
+
+
 
 Buona generazione di certificati!
 
--- dist/genera_cert.py
+++ dist/genera_cert.py
@@ -1,154 +1,29 @@
-from OpenSSL import crypto
-from cryptography import x509
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes, serialization
-from cryptography.x509.oid import NameOID, ExtensionOID
-from cryptography.hazmat.primitives.asymmetric import rsa
-from datetime import datetime, timedelta, timezone
-
 import os
+import subprocess
+import shutil
 
-def generate_key():
-    private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048,
-        backend=default_backend()
-    )
-    return private_key
+# Define the variable for the folder name
+certificato_variable = input("Inserisci il nome del certificato: ")
 
-def generate_csr(key, common_name, organization, country, state, locality, organizational_unit, email):
-    subject = x509.Name([
-        x509.NameAttribute(NameOID.COUNTRY_NAME, country),
-        x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, state),
-        x509.NameAttribute(NameOID.LOCALITY_NAME, locality),
-        x509.NameAttribute(NameOID.ORGANIZATION_NAME, organization),
-        x509.NameAttribute(NameOID.ORGANIZATIONAL_UNIT_NAME, organizational_unit),
-        x509.NameAttribute(NameOID.EMAIL_ADDRESS, email),
-        x509.NameAttribute(NameOID.COMMON_NAME, common_name),
-    ])
+# Convert the variable to lowercase for the folder name
+certificato_folder = os.path.join("..", "certificati", f"certificato-{certificato_variable.lower()}")
 
-    builder = x509.CertificateSigningRequestBuilder().subject_name(subject)
+# Define the paths
+generate_sh_path = os.path.abspath("./generate.sh")
 
-    # Add subjectAltName extension
-    san = x509.SubjectAlternativeName([
-        x509.DNSName("*.localhost"),
-        x509.DNSName("localhost"),
-    ])
-    builder = builder.add_extension(san, critical=False)
+try:
+    # Run the script using Git Bash in the current working directory
+    subprocess.run([r"C:\Program Files\Git\bin\bash.exe", generate_sh_path], check=True)
 
-    csr = builder.sign(key, algorithm=hashes.SHA256(), backend=default_backend())
-    return csr
+    # Create the folder if it doesn't exist inside the root/certificati directory
+    os.makedirs(os.path.join(os.getcwd(), certificato_folder), exist_ok=True)
 
-def generate_certificate(csr, key, folder_name, days_valid=365):
-    builder = x509.CertificateBuilder()
-    builder = builder.subject_name(csr.subject)
-    builder = builder.issuer_name(csr.subject)
-    builder = builder.not_valid_before(datetime.now(timezone.utc))
-    builder = builder.not_valid_after(datetime.now(timezone.utc) + timedelta(days=days_valid))
-    builder = builder.serial_number(x509.random_serial_number())
-    builder = builder.public_key(csr.public_key())
+    # Move the generated files to the folder
+    shutil.move("server.key", os.path.join(certificato_folder, "server.key"))
+    shutil.move("server.crt", os.path.join(certificato_folder, "server.crt"))
 
-    builder = builder.add_extension(
-        x509.BasicConstraints(ca=False, path_length=None), critical=True,
-    )
-
-    # Extract the SubjectAlternativeName extension from the CSR and add it to the certificate
-    san_extension = csr.extensions.get_extension_for_oid(ExtensionOID.SUBJECT_ALTERNATIVE_NAME)
-    builder = builder.add_extension(san_extension.value, critical=False)
-
-    builder = builder.add_extension(
-        x509.SubjectKeyIdentifier.from_public_key(csr.public_key()), critical=False,
-    )
-
-    # Add the SubjectKeyIdentifier extension directly from the key
-    builder = builder.add_extension(
-        x509.AuthorityKeyIdentifier.from_issuer_subject_key_identifier(
-            x509.SubjectKeyIdentifier.from_public_key(key.public_key())
-        ),
-        critical=False,
-    )
-
-    cert = builder.sign(key, hashes.SHA256(), default_backend())
-
-    # Save the private key
-    private_key_pem = key.private_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PrivateFormat.TraditionalOpenSSL,
-        encryption_algorithm=serialization.NoEncryption()
-    )
-    save_to_file(folder_name, "private.key", private_key_pem)
-
-    # Save the certificate
-    cert_pem = cert.public_bytes(encoding=serialization.Encoding.PEM)
-    save_to_file(folder_name, "certificate.crt", cert_pem)
-
-    return cert
-
-def save_to_file(folder, filename, data):
-    os.makedirs(folder, exist_ok=True)
-    with open(os.path.join(folder, filename), "wb") as file:
-        file.write(data)
-
-# Default values
-default_values = {
-    "common_name": "localhost",
-    "organization": "IT",
-    "country": "US",
-    "state": "KS",
-    "locality": "Olathe",
-    "organizational_unit": "IT Department",
-    "email": "webmaster@example.com"
-}
-
-# Function to modify default values
-def modify_default_values():
-    # Now the default_values dictionary is updated based on user input
-    print("Default values:", default_values)
-    print("Modify default values:")
-    for key in default_values:
-        new_value = input(f"Enter new {key} (press Enter to keep default value '{default_values[key]}'): ")
-        if new_value:
-            default_values[key] = new_value
-    return default_values
-
-def main():
-    try:
-        # Call the function to modify default values
-        modify_default_values()
-
-        # Update values based on user input
-        common_name = default_values['common_name']
-        organization = default_values['organization']
-        country = default_values['country']
-        state = default_values['state']
-        locality = default_values['locality']
-        organizational_unit = default_values['organizational_unit']
-        email = default_values['email']
-
-        # Specify the path to the "certificati" folder at the project root level
-        certificati_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "certificati")
-
-        # Construct the folder path inside "certificati"
-        folder_name = os.path.join(certificati_folder, "certificazione-" + common_name)
-
-        # Genera la chiave privata e la CSR come hai fatto prima
-        key = generate_key()
-        csr = generate_csr(
-            key,
-            common_name=common_name,
-            organization=organization,
-            country=country,
-            state=state,
-            locality=locality,
-            organizational_unit=organizational_unit,
-            email=email
-        )
-
-        # Genera il certificato a partire dalla CSR e dalla chiave privata
-        cert = generate_certificate(csr, key, folder_name)  # Pass the folder_name here
-        print("Certificate generation successful.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-if __name__ == "__main__":
-    main()
+    print("SSL certificates generated successfully.")
+except subprocess.CalledProcessError as e:
+    print(f"Error: {e}")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
